f0316bba57d60c6abca96e53969ba6bbbcd377b1
Saul Ricaño Monroy <saulrimso@gmail.com>
2024-05-08 13:25:56 -0600

    Corrección header

--- src/componets/Header.jsx
+++ src/componets/Header.jsx
@@ -5,7 +5,7 @@ export const Header = () => {
             <img className='img-header-csv'
             src="https://i.pinimg.com/236x/2f/97/f0/2f97f05b32547f54ef1bdf99cd207c90.jpg" alt="" />
             <nav className="nav-header-csv">
-                <a href="/">Inicio</a>
+                <a href="#">Inicio</a>
                 <a href="#about">Acerca de</a>
                 <a href="#proyects">Proyectos</a>
                 <a href="#contact">Contacto</a>
