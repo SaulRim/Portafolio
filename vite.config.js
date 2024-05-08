076465aac082607a49fb534ca20ed3d2bd35dbf0
Saul Ricaño Monroy <saulrimso@gmail.com>
2024-05-08 12:57:38 -0600

    Modificaciones de vista

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "https://SaulRim.github.io/Portafolio"
+  base: "https://saulrim.github.io/Portafolio"
 });
